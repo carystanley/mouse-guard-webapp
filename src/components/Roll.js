a8dbf76e7c860a5793e908bf5c245e441e2e2877
Cary Stanley <cary@yahoo-inc.com>
2016-09-28 15:43:03 -0700

    Add ComponentDidMount Scroll

--- src/components/Roll.js
+++ src/components/Roll.js
@@ -1,15 +1,24 @@
 import React, { PropTypes } from 'react'
 
-const Roll = ({ values, successes }) => (
-  <li>
-    <div>{values.map(value => <span className={'dice dice-' + value} />)}</div>
-    <div>{successes} Successes</div>
-  </li>
-)
+const Roll = React.createClass({
+  render: function() {
+    let {values, successes} = this.props;
+    return (
+      <li ref={(ref) => this._li = ref}>
+        <div>{values.map(value => <span className={'dice dice-' + value} />)}</div>
+        <div>{successes} Successes</div>
+      </li>
+    );
+  },
 
-Roll.propTypes = {
-  completed: PropTypes.bool.isRequired,
-  text: PropTypes.string.isRequired
-}
+  componentDidMount: function() {
+    this._li.scrollIntoView({block: "end", behavior: "smooth"});
+  },
+
+  propTypes: {
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+  }
+});
 
 export default Roll
