d4e821b6f6b1ee5f10d55c363fac9a930aac4e3e
Cary Stanley <cary@yahoo-inc.com>
2016-09-29 11:08:43 -0700

    Streamline Generator code

--- src/lib/characterGenerator.js
+++ src/lib/characterGenerator.js
@@ -1,64 +1,42 @@
-
 import config from '../config/character.json';
 
 
 
-var bumpSkill = (state, name) => {
-  return {
-    ...state,
-    Skills: (state.Skills[name]) ? state.Skills[name] + 1 : 2
-  };
-};
+var bumpSkill = (state, name) => ({
+  ...state,
+  Skills: (state.Skills[name]) ? state.Skills[name] + 1 : 2
+});
 
-var bumpTrait = (state, name) => {
-  return {
+var bumpTrait = (state, name) => ({
     ...state,
     Traits: (state.Traits[name]) ? state.Traits[name] + 1 : 1
-  };
-};
+});
 
 
 
 var CharacterGeneratror = [{
   question: 'What is your Guard Rank?',
   choices: (state) => { return config.ranks; },
-  affect: (state, choice) => { 
-    return {
-      ...config.bases.default,
-      ...config.bases[choice],
-      Rank: choice
-    };
-  }
+  affect: (state, choice) => ({
+    ...config.bases.default,
+    ...config.bases[choice],
+    Rank: choice
+  })
 }, {
   question: 'Where were you born?',
-  choices: (state) => {
-    return Object.keys(config.homes).map(function(home) {
-      return home;
-    }); 
-  },
-  affect: (state, choice) => { 
-    return {
-      ...state,
-      Home: choice
-    };
-  }
+  choices: (state) => (Object.keys(config.homes).map(function(home) { return home; })),
+  affect: (state, choice) => ({...state, Home: choice})
 }, {
   question: 'What skill did you learn there?',
-  choices: (state) => {
-    return config.homes[state.Home].skills
-  },
+  choices: (state) => (config.homes[state.Home].skills),
   affect: bumpSkill
 }, {
   question: 'What trait did you inherit there?',
-  choices: (state) => {
-    return config.homes[state.Home].traits
-  },
+  choices: (state) => (config.homes[state.Home].traits),
   affect: bumpTrait
 }, {
   question: 'What is your natural talent?',
-  choices: (state) => {
-    return config.skills
-  },
+  choices: config.skills,
   affect: bumpSkill
 }];
 
