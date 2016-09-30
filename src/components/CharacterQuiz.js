b1b9323d668a4d59aa625b0d10fb34f94ddf183d
Cary Stanley <cary@yahoo-inc.com>
2016-09-30 13:21:47 -0700

    Responsive Layout

--- src/components/CharacterQuiz.js
+++ src/components/CharacterQuiz.js
@@ -3,9 +3,9 @@ import CharacterQuestion from './CharacterQuestion'
 import CharacterSheet from './CharacterSheet'
 
 const CharacterQuiz = (props) => (
-  <div>
-    <CharacterQuestion {...props} />
-    <CharacterSheet {...props} />
+  <div id="layout">
+    <div id="primary"><div className="roomy"><CharacterQuestion {...props} /></div></div>
+    <div id="secondary"><div className="roomy"><CharacterSheet {...props} /></div></div>
   </div>
 )
 
