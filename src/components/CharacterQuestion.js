1f6895c1539fc7610c2427ecf5368ac9659bf12e
Cary Stanley <cary@yahoo-inc.com>
2016-09-30 09:52:46 -0700

    Add Progress Bar

--- src/components/CharacterQuestion.js
+++ src/components/CharacterQuestion.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const CharacterQuestions = ({character, onChoose}) => (
-  <div>
-    <div className="question">{character.question}</div>
-    <div className="choices">{character.choices ? character.choices.map((choice) => <button onClick={() => onChoose(choice)}>{choice}</button>) : null}</div>
-  </div>
+  (character.question) ?
+    <div>
+      <div className="progress-container"><div className="progressbar" style={{width: (character.step / character.totalSteps) * 100 + '%'}}></div></div>
+      <div className="question">{character.question}</div>
+      <div className="choices">{character.choices ? character.choices.map((choice) => <button onClick={() => onChoose(choice)}>{choice}</button>) : null}</div>
+    </div>
+  : null
 )
 
 export default CharacterQuestions
